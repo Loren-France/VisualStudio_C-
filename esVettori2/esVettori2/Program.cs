2867ef76d44706f919c71d3ec90da07bab3e00c9
Loren-France <14529846@itisrossi.vi.it>
2026-02-11 18:56:42 +0100

    Definitivo numeri casuali trovare il doppio

---
+++ esVettori2/esVettori2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace esVettori2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			int[] numeri = new int[30];
+
+			Write("Vettore di 30 numeri casuali compresi tra 1 e 100: ");
+
+			for (int i = 0; i < numeri.Length; i++)
+			{
+				numeri[i] = rnd.Next(0, 101);
+				Write($"{numeri[i]} ");
+			}
+
+			WriteLine("\n");
+			Write("Numeri doppi trovati: ");
+
+			for (int j = 0; j < numeri.Length - 1; j++)
+			{
+				for (int k = j + 1; k < numeri.Length; k++)
+				{
+					if (numeri[j] == numeri[k] * 2)
+					{
+						Write($" {numeri[j]} {numeri[k]} / ");
+					}
+					else if (numeri[k] == numeri[j] * 2)
+					{
+						Write($" {numeri[k]} {numeri[j]} / ");
+					}
+				}
+			}
+		}
+	}
+}
