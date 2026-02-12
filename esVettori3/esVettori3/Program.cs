2225b954ac6e03739f8b7c7cab07d16bbb956038
Loren-France <14529846@itisrossi.vi.it>
2026-02-12 17:47:41 +0100

    Definitivo es vettori 3

---
+++ esVettori3/esVettori3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+
+namespace esVettori3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float[] numeri = new float[99];
+			float somma = 0;
+			bool control;
+			int c = 0;
+
+			WriteLine("Programma che legge una sequenza di numeri e cerca una coppia di numeri con rapporto uguale al primo numero immesso");
+			WriteLine();
+			WriteLine("Inserire i numeri in esame, terminazione della lettura se somma di essi maggiore di 50 o sono più di 100 numeri");
+			WriteLine();
+
+			foreach (int i in numeri)
+			{
+				numeri[i] = 0;
+			}
+
+			while (somma <= 50 && c!=100)
+			{
+				float n;
+				do
+				{
+					Write("Inserire un numero: ");
+					control = float.TryParse(ReadLine(), out n);
+				}
+				while (!control || n == 0);
+
+				somma += n;
+				numeri[c] = n;
+
+				c++;
+			}
+			if (numeri[2] == 0)
+			{
+				WriteLine("Sono stati inseriti meno di 3 numeri, conclusione del programma");
+			}
+			else
+			{
+				for (int j = 0; j < numeri.Length; j++)
+				{
+					for (int k = 0;  k < numeri.Length; k++)
+					{
+						if (j != k && numeri[j] / numeri[k] == numeri[0]) 
+						{
+							WriteLine($"Il rapporto tra {numeri[j]} e {numeri[k]} è uguale a {numeri[0]}");
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- esVettori3/esVettori3/obj/Debug/net10.0/esVettori3.AssemblyInfo.cs
+++ esVettori3/esVettori3/obj/Debug/net10.0/esVettori3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("esVettori1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("esVettori3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2867ef76d44706f919c71d3ec90da07bab3e00c9")]
-[assembly: System.Reflection.AssemblyProductAttribute("esVettori1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("esVettori1")]
+[assembly: System.Reflection.AssemblyProductAttribute("esVettori3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("esVettori3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
