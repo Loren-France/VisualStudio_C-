639923cf6e24e4129c7deea8ec0c3cfb1aa6713b
Loren-France <14529846@itisrossi.vi.it>
2026-02-27 19:52:02 +0100

    Definitivo Creature Magiche

---
+++ EsCreatureMagiche/EsCreatureMagiche/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace EsCreatureMagiche
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Codice generato da Progettazione Windows Form
+
+		/// <summary>
+		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+		/// il contenuto del metodo con l'editor di codice.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.lblTitolo = new System.Windows.Forms.Label();
+			this.lblBoxCreature = new System.Windows.Forms.Label();
+			this.lblMagia = new System.Windows.Forms.Label();
+			this.lblRazza = new System.Windows.Forms.Label();
+			this.lblNome = new System.Windows.Forms.Label();
+			this.btnCreaturaPotente = new System.Windows.Forms.Button();
+			this.btnFiltraRazza = new System.Windows.Forms.Button();
+			this.btnCreatureDeboli = new System.Windows.Forms.Button();
+			this.btnMediaMagia = new System.Windows.Forms.Button();
+			this.btnScaccia = new System.Windows.Forms.Button();
+			this.btnEvoca = new System.Windows.Forms.Button();
+			this.lstBoxCreature = new System.Windows.Forms.ListBox();
+			this.cmbBoxMagia = new System.Windows.Forms.ComboBox();
+			this.cmbBoxRazza = new System.Windows.Forms.ComboBox();
+			this.txtBoxNome = new System.Windows.Forms.TextBox();
+			this.btnAvvia = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.lblTitolo);
+			this.panel1.Controls.Add(this.lblBoxCreature);
+			this.panel1.Controls.Add(this.lblMagia);
+			this.panel1.Controls.Add(this.lblRazza);
+			this.panel1.Controls.Add(this.lblNome);
+			this.panel1.Controls.Add(this.btnCreaturaPotente);
+			this.panel1.Controls.Add(this.btnFiltraRazza);
+			this.panel1.Controls.Add(this.btnCreatureDeboli);
+			this.panel1.Controls.Add(this.btnMediaMagia);
+			this.panel1.Controls.Add(this.btnScaccia);
+			this.panel1.Controls.Add(this.btnEvoca);
+			this.panel1.Controls.Add(this.lstBoxCreature);
+			this.panel1.Controls.Add(this.cmbBoxMagia);
+			this.panel1.Controls.Add(this.cmbBoxRazza);
+			this.panel1.Controls.Add(this.txtBoxNome);
+			this.panel1.Location = new System.Drawing.Point(42, 50);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1098, 803);
+			this.panel1.TabIndex = 0;
+			// 
+			// lblTitolo
+			// 
+			this.lblTitolo.Location = new System.Drawing.Point(480, 25);
+			this.lblTitolo.Name = "lblTitolo";
+			this.lblTitolo.Size = new System.Drawing.Size(141, 25);
+			this.lblTitolo.TabIndex = 29;
+			this.lblTitolo.Text = "Creature Magiche";
+			this.lblTitolo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblBoxCreature
+			// 
+			this.lblBoxCreature.Location = new System.Drawing.Point(21, 55);
+			this.lblBoxCreature.Name = "lblBoxCreature";
+			this.lblBoxCreature.Size = new System.Drawing.Size(416, 23);
+			this.lblBoxCreature.TabIndex = 28;
+			this.lblBoxCreature.Text = "Lista creature:";
+			this.lblBoxCreature.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblMagia
+			// 
+			this.lblMagia.Location = new System.Drawing.Point(923, 322);
+			this.lblMagia.Name = "lblMagia";
+			this.lblMagia.Size = new System.Drawing.Size(124, 38);
+			this.lblMagia.TabIndex = 27;
+			this.lblMagia.Text = "Inserisci il livello di magia:";
+			// 
+			// lblRazza
+			// 
+			this.lblRazza.Location = new System.Drawing.Point(923, 89);
+			this.lblRazza.Name = "lblRazza";
+			this.lblRazza.Size = new System.Drawing.Size(124, 38);
+			this.lblRazza.TabIndex = 26;
+			this.lblRazza.Text = "Inserisci la razza da filtrare:";
+			// 
+			// lblNome
+			// 
+			this.lblNome.Location = new System.Drawing.Point(532, 89);
+			this.lblNome.Name = "lblNome";
+			this.lblNome.Size = new System.Drawing.Size(146, 38);
+			this.lblNome.TabIndex = 25;
+			this.lblNome.Text = "Inserisci il nome della creatura magica:";
+			// 
+			// btnCreaturaPotente
+			// 
+			this.btnCreaturaPotente.Location = new System.Drawing.Point(926, 442);
+			this.btnCreaturaPotente.Name = "btnCreaturaPotente";
+			this.btnCreaturaPotente.Size = new System.Drawing.Size(121, 66);
+			this.btnCreaturaPotente.TabIndex = 24;
+			this.btnCreaturaPotente.Text = "Visualizza più potente";
+			this.btnCreaturaPotente.UseVisualStyleBackColor = true;
+			this.btnCreaturaPotente.Click += new System.EventHandler(this.btnCreaturaPotente_Click);
+			// 
+			// btnFiltraRazza
+			// 
+			this.btnFiltraRazza.Location = new System.Drawing.Point(926, 206);
+			this.btnFiltraRazza.Name = "btnFiltraRazza";
+			this.btnFiltraRazza.Size = new System.Drawing.Size(121, 66);
+			this.btnFiltraRazza.TabIndex = 23;
+			this.btnFiltraRazza.Text = "Filtra per razza";
+			this.btnFiltraRazza.UseVisualStyleBackColor = true;
+			this.btnFiltraRazza.Click += new System.EventHandler(this.btnFiltraRazza_Click);
+			// 
+			// btnCreatureDeboli
+			// 
+			this.btnCreatureDeboli.Location = new System.Drawing.Point(535, 322);
+			this.btnCreatureDeboli.Name = "btnCreatureDeboli";
+			this.btnCreatureDeboli.Size = new System.Drawing.Size(121, 66);
+			this.btnCreatureDeboli.TabIndex = 22;
+			this.btnCreatureDeboli.Text = "Creature deboli";
+			this.btnCreatureDeboli.UseVisualStyleBackColor = true;
+			this.btnCreatureDeboli.Click += new System.EventHandler(this.btnCreatureDeboli_Click);
+			// 
+			// btnMediaMagia
+			// 
+			this.btnMediaMagia.Location = new System.Drawing.Point(726, 322);
+			this.btnMediaMagia.Name = "btnMediaMagia";
+			this.btnMediaMagia.Size = new System.Drawing.Size(121, 66);
+			this.btnMediaMagia.TabIndex = 21;
+			this.btnMediaMagia.Text = "Media Magica";
+			this.btnMediaMagia.UseVisualStyleBackColor = true;
+			this.btnMediaMagia.Click += new System.EventHandler(this.btnMediaMagia_Click);
+			// 
+			// btnScaccia
+			// 
+			this.btnScaccia.Location = new System.Drawing.Point(726, 206);
+			this.btnScaccia.Name = "btnScaccia";
+			this.btnScaccia.Size = new System.Drawing.Size(121, 66);
+			this.btnScaccia.TabIndex = 20;
+			this.btnScaccia.Text = "Scaccia creatura";
+			this.btnScaccia.UseVisualStyleBackColor = true;
+			this.btnScaccia.Click += new System.EventHandler(this.btnScaccia_Click);
+			// 
+			// btnEvoca
+			// 
+			this.btnEvoca.Location = new System.Drawing.Point(535, 206);
+			this.btnEvoca.Name = "btnEvoca";
+			this.btnEvoca.Size = new System.Drawing.Size(121, 66);
+			this.btnEvoca.TabIndex = 19;
+			this.btnEvoca.Text = "Evoca creatura!";
+			this.btnEvoca.UseVisualStyleBackColor = true;
+			this.btnEvoca.Click += new System.EventHandler(this.btnEvoca_Click);
+			// 
+			// lstBoxCreature
+			// 
+			this.lstBoxCreature.FormattingEnabled = true;
+			this.lstBoxCreature.ItemHeight = 16;
+			this.lstBoxCreature.Location = new System.Drawing.Point(21, 81);
+			this.lstBoxCreature.Name = "lstBoxCreature";
+			this.lstBoxCreature.Size = new System.Drawing.Size(416, 644);
+			this.lstBoxCreature.TabIndex = 18;
+			// 
+			// cmbBoxMagia
+			// 
+			this.cmbBoxMagia.FormattingEnabled = true;
+			this.cmbBoxMagia.Location = new System.Drawing.Point(926, 364);
+			this.cmbBoxMagia.Name = "cmbBoxMagia";
+			this.cmbBoxMagia.Size = new System.Drawing.Size(121, 24);
+			this.cmbBoxMagia.TabIndex = 17;
+			// 
+			// cmbBoxRazza
+			// 
+			this.cmbBoxRazza.FormattingEnabled = true;
+			this.cmbBoxRazza.Location = new System.Drawing.Point(926, 130);
+			this.cmbBoxRazza.Name = "cmbBoxRazza";
+			this.cmbBoxRazza.Size = new System.Drawing.Size(121, 24);
+			this.cmbBoxRazza.TabIndex = 16;
+			// 
+			// txtBoxNome
+			// 
+			this.txtBoxNome.Location = new System.Drawing.Point(535, 130);
+			this.txtBoxNome.Name = "txtBoxNome";
+			this.txtBoxNome.Size = new System.Drawing.Size(175, 22);
+			this.txtBoxNome.TabIndex = 15;
+			// 
+			// btnAvvia
+			// 
+			this.btnAvvia.Location = new System.Drawing.Point(525, 12);
+			this.btnAvvia.Name = "btnAvvia";
+			this.btnAvvia.Size = new System.Drawing.Size(138, 47);
+			this.btnAvvia.TabIndex = 30;
+			this.btnAvvia.Text = "Avvia";
+			this.btnAvvia.UseVisualStyleBackColor = true;
+			this.btnAvvia.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1189, 885);
+			this.Controls.Add(this.btnAvvia);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label lblTitolo;
+		private System.Windows.Forms.Label lblBoxCreature;
+		private System.Windows.Forms.Label lblMagia;
+		private System.Windows.Forms.Label lblRazza;
+		private System.Windows.Forms.Label lblNome;
+		private System.Windows.Forms.Button btnCreaturaPotente;
+		private System.Windows.Forms.Button btnFiltraRazza;
+		private System.Windows.Forms.Button btnCreatureDeboli;
+		private System.Windows.Forms.Button btnMediaMagia;
+		private System.Windows.Forms.Button btnScaccia;
+		private System.Windows.Forms.Button btnEvoca;
+		private System.Windows.Forms.ListBox lstBoxCreature;
+		private System.Windows.Forms.ComboBox cmbBoxMagia;
+		private System.Windows.Forms.ComboBox cmbBoxRazza;
+		private System.Windows.Forms.TextBox txtBoxNome;
+		private System.Windows.Forms.Button btnAvvia;
+	}
+}
+
---
+++ EsCreatureMagiche/EsCreatureMagiche/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsCreatureMagiche
+{
+	public enum Razza
+	{
+		Drago,
+		Elfo,
+		Troll,
+		Fata
+	}
+
+	public struct Creatura
+	{
+		public string Nome;
+		public Razza Razza;
+		public int LivelloMagia;
+		public Creatura(string nome, Razza razza, int livelloMagia)
+		{
+			Nome = nome;
+			Razza = razza;
+			LivelloMagia = livelloMagia;
+		}
+		public string ToString()
+		{
+			return $"Nome: {Nome} - Razza: {Razza} - Magia: {LivelloMagia}";
+		}
+	}
+	public partial class Form1 : Form
+	{
+		static List<Creatura> creatures = new List<Creatura>();
+		public Form1()
+		{
+			InitializeComponent();
+			cmbBoxRazza.DataSource = Enum.GetValues(typeof(Razza));
+			for (int i = 1; i <= 10; i++)
+			{
+				cmbBoxMagia.Items.Add(i);
+			}
+			cmbBoxMagia.SelectedIndex = 0;
+			panel1.Visible = false;
+		}
+
+		private void btnEvoca_Click(object sender, EventArgs e)
+		{
+			Creatura creatura = new Creatura();
+
+			string name = txtBoxNome.Text;
+			Razza razza = (Razza)cmbBoxRazza.SelectedItem;
+			int magia = 1;
+			if (cmbBoxMagia.SelectedItem != null)
+			{
+				magia = (int)cmbBoxMagia.SelectedItem;
+			}
+
+			creatura.Nome = name;
+			creatura.Razza = razza;
+			creatura.LivelloMagia = magia;
+
+			if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+			{
+				MessageBox.Show("Spiacente, inserire un nome valido");
+				return;
+			}
+
+			creatures.Add(creatura);
+
+			lstBoxCreature.Items.Clear();
+
+			for (int i = 0; i < creatures.Count(); i++)
+			{
+				lstBoxCreature.Items.Add(creatures[i].ToString());
+			}
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			btnAvvia.Visible = false;
+			panel1.Visible = true;
+		}
+
+		private void btnScaccia_Click(object sender, EventArgs e)
+		{
+			string rimuovere = txtBoxNome.Text;
+
+			if (string.IsNullOrEmpty(rimuovere) || string.IsNullOrWhiteSpace(rimuovere))
+			{
+				MessageBox.Show("Spiacente, inserire un nome valido");
+			}
+
+			for (int i = 0; i < creatures.Count(); i++)
+			{
+				if (creatures[i].Nome == rimuovere)
+				{
+					creatures.RemoveAt(i);
+					break;
+				}
+			}
+
+			lstBoxCreature.Items.Clear();
+
+			for (int i = 0; i < creatures.Count(); i++)
+			{
+				lstBoxCreature.Items.Add(creatures[i].ToString());
+			}
+		}
+
+		private void btnMediaMagia_Click(object sender, EventArgs e)
+		{
+			double media = 0;
+			int somma = 0;
+			int c = 0;
+
+			for (c = 0; c < creatures.Count; c++)
+			{
+				somma += creatures[c].LivelloMagia;
+			}
+
+			if (c > 0)
+			{
+				media = (double)somma / c;
+				MessageBox.Show($"La media del livello di magia è: {media}");
+			}
+			else
+			{
+				MessageBox.Show("Non ci sono creature evocate sufficienti per fare la media dei livelli di magia");
+			}
+		}
+
+		private void btnCreatureDeboli_Click(object sender, EventArgs e)
+		{
+			int deboli = 0;
+
+			for (int j = 0; j < creatures.Count; j++)
+			{
+				if (creatures[j].LivelloMagia < 5)
+				{
+					deboli++;
+				}
+			}
+
+			if (deboli > 0)
+			{
+				MessageBox.Show($"Ci sono {deboli} creature con un livello di magia inferiore a 5");
+			}
+			else
+			{
+				MessageBox.Show("Non ci sono creature evocate con un livello di magia inferiore a 5, oppure non ci sono mostri su cui verificare la debolezza");
+			}
+		}
+
+		private void btnFiltraRazza_Click(object sender, EventArgs e)
+		{
+			Razza race = (Razza)cmbBoxRazza.SelectedItem;
+
+			lstBoxCreature.Items.Clear();
+
+			MessageBox.Show($"Lista filtrata per la razza: {race}");
+
+			for (int i = 0; i < creatures.Count(); i++)
+			{
+				if (creatures[i].Razza == race)
+				{
+					lstBoxCreature.Items.Add(creatures[i].ToString());
+				}
+			}
+		}
+
+		private void btnCreaturaPotente_Click(object sender, EventArgs e)
+		{
+			Creatura first = creatures[0];
+
+			for (int i = 1; i < creatures.Count(); i++)
+			{
+				if (creatures[i].LivelloMagia > first.LivelloMagia)
+				{
+					first = creatures[i];
+				}
+			}
+
+			if (creatures.Count() > 0)
+			{
+				MessageBox.Show($"La creatura più potente è = {first.ToString()}");
+			}
+			else
+			{
+				MessageBox.Show("Non ci sono creature evocate sufficienti per identificare la creatura più potente");
+			}
+		}
+	}
+}
