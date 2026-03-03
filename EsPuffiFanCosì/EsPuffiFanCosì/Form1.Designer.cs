f2319b6cd5e4b103c868b485551bad23dba789b3
Loren-France <14529846@itisrossi.vi.it>
2026-03-03 18:25:08 +0100

    Definitivo Puffi fa cosi

--- EsPuffiFanCosì/EsPuffiFanCosì/Form1.Designer.cs
+++ EsPuffiFanCosì/EsPuffiFanCosì/Form1.Designer.cs
@@ -28,53 +28,99 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.lblPunteggioG = new System.Windows.Forms.Label();
+			this.lblPunteggioP = new System.Windows.Forms.Label();
+			this.lblTurno = new System.Windows.Forms.Label();
 			this.pnlCittà = new System.Windows.Forms.Panel();
+			this.pnlGargamella = new System.Windows.Forms.Panel();
 			this.pnlCasetta = new System.Windows.Forms.Panel();
 			this.pnlPuffo = new System.Windows.Forms.Panel();
-			this.lblPunteggio = new System.Windows.Forms.Label();
 			this.pnlCittà.SuspendLayout();
 			this.SuspendLayout();
 			// 
+			// lblPunteggioG
+			// 
+			this.lblPunteggioG.BackColor = System.Drawing.Color.Lime;
+			this.lblPunteggioG.Location = new System.Drawing.Point(950, 0);
+			this.lblPunteggioG.Name = "lblPunteggioG";
+			this.lblPunteggioG.Size = new System.Drawing.Size(177, 24);
+			this.lblPunteggioG.TabIndex = 3;
+			this.lblPunteggioG.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblPunteggioP
+			// 
+			this.lblPunteggioP.BackColor = System.Drawing.Color.Lime;
+			this.lblPunteggioP.Location = new System.Drawing.Point(-2, 0);
+			this.lblPunteggioP.Name = "lblPunteggioP";
+			this.lblPunteggioP.Size = new System.Drawing.Size(141, 24);
+			this.lblPunteggioP.TabIndex = 2;
+			this.lblPunteggioP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lblTurno
+			// 
+			this.lblTurno.BackColor = System.Drawing.Color.Lime;
+			this.lblTurno.Location = new System.Drawing.Point(145, 0);
+			this.lblTurno.Name = "lblTurno";
+			this.lblTurno.Size = new System.Drawing.Size(799, 24);
+			this.lblTurno.TabIndex = 4;
+			this.lblTurno.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
 			// pnlCittà
 			// 
+			this.pnlCittà.BackgroundImage = global::EsPuffiFanCosì.Properties.Resources.Copilot_20260303_182211;
+			this.pnlCittà.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pnlCittà.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pnlCittà.Controls.Add(this.pnlGargamella);
 			this.pnlCittà.Controls.Add(this.pnlCasetta);
 			this.pnlCittà.Controls.Add(this.pnlPuffo);
-			this.pnlCittà.Controls.Add(this.lblPunteggio);
-			this.pnlCittà.Location = new System.Drawing.Point(12, 12);
+			this.pnlCittà.Location = new System.Drawing.Point(1, 27);
 			this.pnlCittà.Name = "pnlCittà";
-			this.pnlCittà.Size = new System.Drawing.Size(1089, 685);
+			this.pnlCittà.Size = new System.Drawing.Size(1126, 693);
 			this.pnlCittà.TabIndex = 0;
+			// 
+			// pnlGargamella
+			// 
+			this.pnlGargamella.BackColor = System.Drawing.Color.Black;
+			this.pnlGargamella.BackgroundImage = global::EsPuffiFanCosì.Properties.Resources.gargamel_smurfs_the_lost_village_11546805852mrtajxrxqp;
+			this.pnlGargamella.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pnlGargamella.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pnlGargamella.Location = new System.Drawing.Point(627, 393);
+			this.pnlGargamella.Name = "pnlGargamella";
+			this.pnlGargamella.Size = new System.Drawing.Size(84, 107);
+			this.pnlGargamella.TabIndex = 2;
 			// 
 			// pnlCasetta
 			// 
 			this.pnlCasetta.BackColor = System.Drawing.Color.Red;
-			this.pnlCasetta.Location = new System.Drawing.Point(295, 240);
+			this.pnlCasetta.BackgroundImage = global::EsPuffiFanCosì.Properties.Resources.fecc1bda2a3cf7c6e5c858d602bb32b7;
+			this.pnlCasetta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pnlCasetta.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pnlCasetta.Location = new System.Drawing.Point(375, 237);
 			this.pnlCasetta.Name = "pnlCasetta";
-			this.pnlCasetta.Size = new System.Drawing.Size(52, 60);
+			this.pnlCasetta.Size = new System.Drawing.Size(84, 107);
 			this.pnlCasetta.TabIndex = 1;
 			// 
 			// pnlPuffo
 			// 
 			this.pnlPuffo.BackColor = System.Drawing.Color.Blue;
-			this.pnlPuffo.Location = new System.Drawing.Point(736, 172);
+			this.pnlPuffo.BackgroundImage = global::EsPuffiFanCosì.Properties.Resources.brainy_smurf_11530967289fgqvkfukzv;
+			this.pnlPuffo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pnlPuffo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pnlPuffo.Location = new System.Drawing.Point(741, 112);
 			this.pnlPuffo.Name = "pnlPuffo";
-			this.pnlPuffo.Size = new System.Drawing.Size(52, 60);
+			this.pnlPuffo.Size = new System.Drawing.Size(84, 107);
 			this.pnlPuffo.TabIndex = 0;
-			// 
-			// lblPunteggio
-			// 
-			this.lblPunteggio.BackColor = System.Drawing.SystemColors.Control;
-			this.lblPunteggio.Location = new System.Drawing.Point(0, 0);
-			this.lblPunteggio.Name = "lblPunteggio";
-			this.lblPunteggio.Size = new System.Drawing.Size(124, 24);
-			this.lblPunteggio.TabIndex = 2;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1113, 709);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.ClientSize = new System.Drawing.Size(1130, 723);
+			this.Controls.Add(this.lblTurno);
 			this.Controls.Add(this.pnlCittà);
+			this.Controls.Add(this.lblPunteggioP);
+			this.Controls.Add(this.lblPunteggioG);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
@@ -88,7 +134,10 @@
 		private System.Windows.Forms.Panel pnlCittà;
 		private System.Windows.Forms.Panel pnlPuffo;
 		private System.Windows.Forms.Panel pnlCasetta;
-		private System.Windows.Forms.Label lblPunteggio;
+		private System.Windows.Forms.Label lblPunteggioP;
+		private System.Windows.Forms.Panel pnlGargamella;
+		private System.Windows.Forms.Label lblPunteggioG;
+		private System.Windows.Forms.Label lblTurno;
 	}
 }
 
--- EsPuffiFanCosì/EsPuffiFanCosì/Form1.cs
+++ EsPuffiFanCosì/EsPuffiFanCosì/Form1.cs
@@ -26,10 +26,14 @@ namespace EsPuffiFanCosì
 	}
 	public partial class Form1 : Form
 	{
+	    Panel[] panels = new Panel[10];
 		Punto puffo = new Punto();
 		Punto casetta = new Punto();
+		Punto gargamella = new Punto();
+		Punto[] alberi = new Punto[10];
 		Random rand = new Random();
-		int c = 0;
+		int cP = 0, cG = 0, movement = 0;
+		bool TurnoPuffo = true;
 
 		public Form1()
 		{
@@ -38,64 +42,181 @@ namespace EsPuffiFanCosì
 			puffo.y = 0;
 			casetta.x = rand.Next(0, pnlCittà.Width - pnlCasetta.Width);
 			casetta.y = rand.Next(0, pnlCittà.Height - pnlCasetta.Height);
+			gargamella.x = pnlCittà.Width - pnlGargamella.Width;
+			gargamella.y = pnlCittà.Height- pnlGargamella.Height;
 			pnlPuffo.Location = new Point(puffo.x, puffo.y);
 			pnlCasetta.Location = new Point(casetta.x, casetta.y);
-			lblPunteggio.Text = $"Punteggio: {c}";
+			pnlGargamella.Location = new Point(gargamella.x, gargamella.y);
+			for (int i = 0; i < panels.Length; i++)
+			{
+				panels[i] = new Panel();
+				panels[i].Width = pnlCasetta.Width;
+				panels[i].Height = pnlCasetta.Height;
+				panels[i].BackColor = Color.Green;
+				panels[i].BackgroundImage = Properties.Resources.a_tree_with_green_leaves_free_vector;
+				panels[i].BackgroundImageLayout = ImageLayout.Zoom;
+				panels[i].BorderStyle = BorderStyle.Fixed3D;
+				panels[i].Visible = true;
+				alberi[i].x = rand.Next(0, pnlCittà.Width - panels[i].Width);
+				alberi[i].y = rand.Next(0, pnlCittà.Height - panels[i].Height);
+				panels[i].Location = new Point(alberi[i].x, alberi[i].y);
+				pnlCittà.Controls.Add(panels[i]);
+			}
+			lblPunteggioP.Text = $"Punteggio Puffo: {cP}";
+			lblPunteggioG.Text = $"Punteggio Gargamella: {cG}";
+			lblTurno.Text = "Turno del PUFFO";
+			lblTurno.ForeColor = Color.Blue;
 		}
 
 		private void Form1_KeyDown(object sender, KeyEventArgs e)
 		{
-			switch(e.KeyCode)
+			if (TurnoPuffo)
 			{
-				case Keys.S:
-					puffo.y += 20;
-					break;
-				case Keys.D:
-					puffo.x += 20;
-					break;
-				case Keys.A:
-					puffo.x -= 20;
-					break;
-				case Keys.W:
-					puffo.y -= 20;
-					break;
-				default:
-					break;
+				lblTurno.Text = "Turno del PUFFO";
+				lblTurno.ForeColor = Color.Blue;
+				switch (e.KeyCode)
+				{
+					case Keys.S:
+						puffo.y += 20;
+						break;
+					case Keys.D:
+						puffo.x += 20;
+						break;
+					case Keys.A:
+						puffo.x -= 20;
+						break;
+					case Keys.W:
+						puffo.y -= 20;
+						break;
+					default:
+						break;
+				}
+
+				if (puffo.x < 0)
+				{
+					puffo.x = 0;
+				}
+				if (puffo.y < 0)
+				{
+					puffo.y = 0;
+				}
+				if (puffo.x > pnlCittà.Width - pnlPuffo.Width)
+				{
+					puffo.x = pnlCittà.Width - pnlPuffo.Width;
+				}
+				if (puffo.y > pnlCittà.Height - pnlPuffo.Height)
+				{
+					puffo.y = pnlCittà.Height - pnlPuffo.Height;
+				}
+
+				pnlPuffo.Location = new Point(puffo.x, puffo.y);
+			}
+			else
+			{
+				lblTurno.Text = "Turno di GARGAMELLA";
+				lblTurno.ForeColor = Color.Red;
+				switch (e.KeyCode)
+				{
+					case Keys.K:
+						gargamella.y += 20;
+						break;
+					case Keys.L:
+						gargamella.x += 20;
+						break;
+					case Keys.J:
+						gargamella.x -= 20;
+						break;
+					case Keys.I:
+						gargamella.y -= 20;
+						break;
+					default:
+						break;
+				}
+
+				if (gargamella.x < 0)
+				{
+					gargamella.x = 0;
+				}
+				if (gargamella.y < 0)
+				{
+					gargamella.y = 0;
+				}
+				if (gargamella.x > pnlCittà.Width - pnlGargamella.Width)
+				{
+					gargamella.x = pnlCittà.Width - pnlGargamella.Width;
+				}
+				if (gargamella.y > pnlCittà.Height - pnlGargamella.Height)
+				{
+					gargamella.y = pnlCittà.Height - pnlGargamella.Height;
+				}
+
+				pnlGargamella.Location = new Point(gargamella.x, gargamella.y);
 			}
 
-			if (puffo.x < 0)
+			if (CollisioneCasetta())
 			{
-				puffo.x = 0;
-			}
-			if (puffo.y < 0)
-			{
-				puffo.y = 0;
-			}
-			if (puffo.x > pnlCittà.Width - pnlPuffo.Width)
-			{
-				puffo.x = pnlCittà.Width - pnlPuffo.Width;
-			}
-			if (puffo.y > pnlCittà.Height - pnlPuffo.Height)
-			{
-				puffo.y = pnlCittà.Height - pnlPuffo.Height;
-			}
-			pnlPuffo.Location = new Point(puffo.x, puffo.y);
-
-			if (Collisione())
-			{
-				c++;
-				lblPunteggio.Text = $"Punteggio: {c}";
+				cP++;
+				lblPunteggioP.Text = $"Punteggio Puffo: {cP}";
 				casetta.x = rand.Next(0, pnlCittà.Width - pnlCasetta.Width);
 				casetta.y = rand.Next(0, pnlCittà.Height - pnlCasetta.Height);
 				pnlCasetta.Location = new Point(casetta.x, casetta.y);
 			}
+
+			if (CollisionePuffo())
+			{
+				cG++;
+				lblPunteggioG.Text = $"Punteggio Gargamella: {cG}";
+				puffo.x = rand.Next(0, pnlCittà.Width - pnlPuffo.Width);
+				puffo.y = rand.Next(0, pnlCittà.Height - pnlPuffo.Height);
+				pnlPuffo.Location = new Point(puffo.x, puffo.y);
+			}
+
+			if (CollisioneAlbero())
+			{
+				for (int k = 0; k < 10; k++)
+				{
+					alberi[k].x = rand.Next(0, pnlCittà.Width - panels[k].Width);
+					alberi[k].y = rand.Next(0, pnlCittà.Height - panels[k].Height);
+					panels[k].Location = new Point(alberi[k].x, alberi[k].y);
+				}
+				TurnoPuffo = !TurnoPuffo;
+				movement = 0;
+			}
+
+			movement++;
+
+			if (movement > 11)
+			{
+				TurnoPuffo = !TurnoPuffo;
+				movement = 0;
+			}
 		}
 
-		private bool Collisione()
+		public bool CollisioneCasetta()
 		{
 			if (puffo.x < casetta.x + pnlCasetta.Width && puffo.x + pnlPuffo.Width > casetta.x && puffo.y < casetta.y + pnlCasetta.Height && puffo.y + pnlPuffo.Height > casetta.y)
 			{
 				return true;
+			}
+			return false;
+		}
+		public bool CollisionePuffo()
+		{
+			if (gargamella.x < puffo.x + pnlPuffo.Width && gargamella.x + pnlGargamella.Width > puffo.x && gargamella.y < puffo.y + pnlPuffo.Height && gargamella.y + pnlGargamella.Height > puffo.y)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool CollisioneAlbero()
+		{
+			for (int j = 0; j < panels.Length; j++)
+			{
+				if ((puffo.x < alberi[j].x + panels[j].Width && puffo.x + pnlPuffo.Width > alberi[j].x && puffo.y < alberi[j].y + panels[j].Height && puffo.y + pnlPuffo.Height > alberi[j].y) || (gargamella.x < alberi[j].x + panels[j].Width && gargamella.x + pnlGargamella.Width > alberi[j].x && gargamella.y < alberi[j].y + panels[j].Height && gargamella.y + pnlGargamella.Height > alberi[j].y))
+				{
+					return true;
+				}
 			}
 			return false;
 		}
