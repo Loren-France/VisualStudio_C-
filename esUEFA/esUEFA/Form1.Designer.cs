65e9fc8ba4b32edb0a0c99d6387c17942fa7baeb
Loren-France <14529846@itisrossi.vi.it>
2026-03-16 18:24:57 +0100

    Definito UEFA

---
+++ esUEFA/esUEFA/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace esUEFA
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Codice generato da Progettazione Windows Form
+
+		/// <summary>
+		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+		/// il contenuto del metodo con l'editor di codice.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lstPartite = new System.Windows.Forms.ListBox();
+			this.btnAggiunta = new System.Windows.Forms.Button();
+			this.txtCasa = new System.Windows.Forms.TextBox();
+			this.txtOspite = new System.Windows.Forms.TextBox();
+			this.lblCasa = new System.Windows.Forms.Label();
+			this.lblOspite = new System.Windows.Forms.Label();
+			this.lblGol2 = new System.Windows.Forms.Label();
+			this.lblGol1 = new System.Windows.Forms.Label();
+			this.txtGol2 = new System.Windows.Forms.TextBox();
+			this.txtGol1 = new System.Windows.Forms.TextBox();
+			this.btnFile = new System.Windows.Forms.Button();
+			this.btnStatistiche = new System.Windows.Forms.Button();
+			this.btnFiltra = new System.Windows.Forms.Button();
+			this.txtFiltra = new System.Windows.Forms.TextBox();
+			this.lblFiltra = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lstPartite
+			// 
+			this.lstPartite.FormattingEnabled = true;
+			this.lstPartite.ItemHeight = 16;
+			this.lstPartite.Location = new System.Drawing.Point(12, 220);
+			this.lstPartite.Name = "lstPartite";
+			this.lstPartite.Size = new System.Drawing.Size(834, 340);
+			this.lstPartite.TabIndex = 0;
+			// 
+			// btnAggiunta
+			// 
+			this.btnAggiunta.Location = new System.Drawing.Point(568, 35);
+			this.btnAggiunta.Name = "btnAggiunta";
+			this.btnAggiunta.Size = new System.Drawing.Size(117, 50);
+			this.btnAggiunta.TabIndex = 1;
+			this.btnAggiunta.Text = "Aggiungi partita";
+			this.btnAggiunta.UseVisualStyleBackColor = true;
+			this.btnAggiunta.Click += new System.EventHandler(this.btnAggiunta_Click);
+			// 
+			// txtCasa
+			// 
+			this.txtCasa.Location = new System.Drawing.Point(30, 63);
+			this.txtCasa.Name = "txtCasa";
+			this.txtCasa.Size = new System.Drawing.Size(139, 22);
+			this.txtCasa.TabIndex = 2;
+			this.txtCasa.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCasa_KeyPress);
+			// 
+			// txtOspite
+			// 
+			this.txtOspite.Location = new System.Drawing.Point(284, 63);
+			this.txtOspite.Name = "txtOspite";
+			this.txtOspite.Size = new System.Drawing.Size(139, 22);
+			this.txtOspite.TabIndex = 3;
+			this.txtOspite.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtOspite_KeyPress);
+			// 
+			// lblCasa
+			// 
+			this.lblCasa.AutoSize = true;
+			this.lblCasa.Location = new System.Drawing.Point(27, 44);
+			this.lblCasa.Name = "lblCasa";
+			this.lblCasa.Size = new System.Drawing.Size(108, 16);
+			this.lblCasa.TabIndex = 4;
+			this.lblCasa.Text = "Squadra in casa:";
+			// 
+			// lblOspite
+			// 
+			this.lblOspite.AutoSize = true;
+			this.lblOspite.Location = new System.Drawing.Point(281, 44);
+			this.lblOspite.Name = "lblOspite";
+			this.lblOspite.Size = new System.Drawing.Size(115, 16);
+			this.lblOspite.TabIndex = 5;
+			this.lblOspite.Text = "Squadra in ospite:";
+			// 
+			// lblGol2
+			// 
+			this.lblGol2.AutoSize = true;
+			this.lblGol2.Location = new System.Drawing.Point(281, 119);
+			this.lblGol2.Name = "lblGol2";
+			this.lblGol2.Size = new System.Drawing.Size(84, 16);
+			this.lblGol2.TabIndex = 9;
+			this.lblGol2.Text = "Gol in ospite:";
+			// 
+			// lblGol1
+			// 
+			this.lblGol1.AutoSize = true;
+			this.lblGol1.Location = new System.Drawing.Point(27, 119);
+			this.lblGol1.Name = "lblGol1";
+			this.lblGol1.Size = new System.Drawing.Size(77, 16);
+			this.lblGol1.TabIndex = 8;
+			this.lblGol1.Text = "Gol in casa:";
+			// 
+			// txtGol2
+			// 
+			this.txtGol2.Location = new System.Drawing.Point(284, 138);
+			this.txtGol2.Name = "txtGol2";
+			this.txtGol2.Size = new System.Drawing.Size(139, 22);
+			this.txtGol2.TabIndex = 7;
+			this.txtGol2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGol2_KeyPress);
+			// 
+			// txtGol1
+			// 
+			this.txtGol1.Location = new System.Drawing.Point(30, 138);
+			this.txtGol1.Name = "txtGol1";
+			this.txtGol1.Size = new System.Drawing.Size(139, 22);
+			this.txtGol1.TabIndex = 6;
+			this.txtGol1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGol1_KeyPress);
+			// 
+			// btnFile
+			// 
+			this.btnFile.Location = new System.Drawing.Point(568, 110);
+			this.btnFile.Name = "btnFile";
+			this.btnFile.Size = new System.Drawing.Size(117, 50);
+			this.btnFile.TabIndex = 10;
+			this.btnFile.Text = "Salva in un file";
+			this.btnFile.UseVisualStyleBackColor = true;
+			this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+			// 
+			// btnStatistiche
+			// 
+			this.btnStatistiche.Location = new System.Drawing.Point(714, 35);
+			this.btnStatistiche.Name = "btnStatistiche";
+			this.btnStatistiche.Size = new System.Drawing.Size(117, 50);
+			this.btnStatistiche.TabIndex = 11;
+			this.btnStatistiche.Text = "Controlla statistiche";
+			this.btnStatistiche.UseVisualStyleBackColor = true;
+			this.btnStatistiche.Click += new System.EventHandler(this.btnStatistiche_Click);
+			// 
+			// btnFiltra
+			// 
+			this.btnFiltra.Location = new System.Drawing.Point(714, 151);
+			this.btnFiltra.Name = "btnFiltra";
+			this.btnFiltra.Size = new System.Drawing.Size(117, 50);
+			this.btnFiltra.TabIndex = 12;
+			this.btnFiltra.Text = "FIltra per squadra";
+			this.btnFiltra.UseVisualStyleBackColor = true;
+			this.btnFiltra.Click += new System.EventHandler(this.btnFiltra_Click);
+			// 
+			// txtFiltra
+			// 
+			this.txtFiltra.Location = new System.Drawing.Point(714, 112);
+			this.txtFiltra.Name = "txtFiltra";
+			this.txtFiltra.Size = new System.Drawing.Size(116, 22);
+			this.txtFiltra.TabIndex = 13;
+			this.txtFiltra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFiltra_KeyPress);
+			// 
+			// lblFiltra
+			// 
+			this.lblFiltra.AutoSize = true;
+			this.lblFiltra.Location = new System.Drawing.Point(711, 93);
+			this.lblFiltra.Name = "lblFiltra";
+			this.lblFiltra.Size = new System.Drawing.Size(120, 16);
+			this.lblFiltra.TabIndex = 14;
+			this.lblFiltra.Text = "Squadra da filtrare:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(858, 572);
+			this.Controls.Add(this.lblFiltra);
+			this.Controls.Add(this.txtFiltra);
+			this.Controls.Add(this.btnFiltra);
+			this.Controls.Add(this.btnStatistiche);
+			this.Controls.Add(this.btnFile);
+			this.Controls.Add(this.lblGol2);
+			this.Controls.Add(this.lblGol1);
+			this.Controls.Add(this.txtGol2);
+			this.Controls.Add(this.txtGol1);
+			this.Controls.Add(this.lblOspite);
+			this.Controls.Add(this.lblCasa);
+			this.Controls.Add(this.txtOspite);
+			this.Controls.Add(this.txtCasa);
+			this.Controls.Add(this.btnAggiunta);
+			this.Controls.Add(this.lstPartite);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lstPartite;
+		private System.Windows.Forms.Button btnAggiunta;
+		private System.Windows.Forms.TextBox txtCasa;
+		private System.Windows.Forms.TextBox txtOspite;
+		private System.Windows.Forms.Label lblCasa;
+		private System.Windows.Forms.Label lblOspite;
+		private System.Windows.Forms.Label lblGol2;
+		private System.Windows.Forms.Label lblGol1;
+		private System.Windows.Forms.TextBox txtGol2;
+		private System.Windows.Forms.TextBox txtGol1;
+		private System.Windows.Forms.Button btnFile;
+		private System.Windows.Forms.Button btnStatistiche;
+		private System.Windows.Forms.Button btnFiltra;
+		private System.Windows.Forms.TextBox txtFiltra;
+		private System.Windows.Forms.Label lblFiltra;
+	}
+}
+
---
+++ esUEFA/esUEFA/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace esUEFA
+{
+	public struct Partita
+	{
+		public string Casa;
+		public string Ospite;
+		public int GolCasa;
+		public int GolOspite;
+
+		public Partita (string Casa, string Ospite, int GolCasa,  int GolOspite)
+		{
+			this.Casa = Casa;
+			this.Ospite = Ospite;
+			this.GolCasa = GolCasa;
+			this.GolOspite = GolOspite;
+		}
+
+		public string Stampa () 
+		{
+			return $"Squadra in casa: {Casa} - Squadra in ospite: {Ospite} - Gol in casa: {GolCasa} - Gol in ospite: {GolOspite}";
+		}
+	}
+
+	public partial class Form1 : Form
+	{
+		private void txtCasa_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char c = e.KeyChar;
+
+			if (!char.IsLetter(c) && !char.IsControl(c))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void txtOspite_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char c = e.KeyChar;
+
+			if (!char.IsLetter(c) && !char.IsControl(c))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void txtFiltra_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char c = e.KeyChar;
+
+			if (!char.IsLetter(c) && !char.IsControl(c))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void txtGol1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char c = e.KeyChar;
+
+			if (!char.IsNumber(c) && !char.IsControl(c) && c != '-')
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void txtGol2_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char c = e.KeyChar;
+
+			if (!char.IsNumber(c) && !char.IsControl(c) && c != '-')
+			{
+				e.Handled = true;
+			}
+		}
+
+		List<Partita> partite = new List<Partita>();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			string fullPath = @"C:\Users\Loren\OneDrive\Documenti\Informatica\VisualStudio\esUEFA\Registro.txt";
+
+			char pv = '-';
+			char capo = '\n';
+
+			using (var sw = new StreamReader(fullPath, true))
+			{
+				string[] righe = sw.ReadToEnd().Split(capo);
+
+				for (int i = 0; i < righe.Length; i++)
+				{
+					string[] dati = righe[i].Split(pv);
+					Partita game = new Partita();
+					game.Casa = dati[0];
+					game.Ospite = dati[1];
+					game.GolCasa = int.Parse(dati[2]);
+					game.GolOspite = int.Parse(dati[3]);
+					partite.Add(game);
+				}
+
+				MessageBox.Show($"Importato da {fullPath}");
+
+				sw.Close();
+
+
+				lstPartite.Items.Clear();
+
+				for (int i = 0; i < partite.Count; i++)
+				{
+					lstPartite.Items.Add(partite[i].Stampa());
+				}
+			}
+		}
+		private void btnAggiunta_Click(object sender, EventArgs e)
+		{
+			Partita game = new Partita ();
+
+			if (txtCasa.Text != txtOspite.Text && !string.IsNullOrEmpty(txtCasa.Text) && !string.IsNullOrEmpty(txtOspite.Text) && !string.IsNullOrEmpty(txtGol1.Text) && !string.IsNullOrEmpty(txtGol2.Text))
+			{
+				game.Casa = txtCasa.Text;
+				game.Ospite = txtOspite.Text;
+				game.GolCasa = int.Parse(txtGol1.Text);
+				game.GolOspite = int.Parse(txtGol2.Text);
+				partite.Add (game);
+			}
+			else
+			{
+				MessageBox.Show("Errore, non tutti i campi compilati sono corretti, reinserire i campi esatti");
+				return;
+			}
+
+			lstPartite.Items.Clear ();
+
+			for (int i = 0; i < partite.Count; i++)
+			{
+				lstPartite.Items.Add(partite[i].Stampa());
+			}
+		}
+
+		private void btnFile_Click(object sender, EventArgs e)
+		{
+			string fullPath = @"C:\Users\Loren\OneDrive\Documenti\Informatica\VisualStudio\esUEFA\Registro.txt";
+
+			using (var sw = new StreamWriter(fullPath, true))
+			{
+
+				for (int i = 0; i < partite.Count; i++)
+				{
+					if (!string.IsNullOrEmpty(partite[i].Casa) && !string.IsNullOrEmpty(partite[i].Ospite) && !string.IsNullOrEmpty(partite[i].GolCasa.ToString()) && !string.IsNullOrEmpty(partite[i].GolOspite.ToString()))
+					{
+						string nomeCasa = partite[i].Casa;
+						string nomeOspite = partite[i].Ospite;
+						int GolCasa = partite[i].GolCasa;
+						int GolOspite = partite[i].GolOspite;
+						sw.WriteLine($"{nomeCasa}-{nomeOspite}-{GolCasa}-{GolOspite}");
+					}
+				}
+			}
+			
+			MessageBox.Show($"Salvato in {fullPath}");
+		}
+
+		private void btnStatistiche_Click(object sender, EventArgs e)
+		{
+			List<string> squadre = new List<string>();
+			List<int> score = new List<int>();
+			string migliore = "";
+			int somma = 0, temp = 0;
+			Partita maggiore = new Partita();
+
+			for (int j = 0; j < partite.Count; j++)
+			{
+				if (!squadre.Contains(partite[j].Casa))
+				{
+					squadre.Add(partite[j].Casa);
+					score.Add(0);
+				}
+				if (!squadre.Contains(partite[j].Ospite))
+				{
+					squadre.Add(partite[j].Ospite);
+					score.Add(0);
+				}
+			}
+
+			for (int k = 0; k < partite.Count; k++)
+			{
+				int indexCasa = squadre.IndexOf(partite[k].Casa);
+				int indexOspite = squadre.IndexOf(partite[k].Ospite);
+
+				score[indexCasa] += partite[k].GolCasa;
+				score[indexOspite] += partite[k].GolOspite;
+			}
+
+			for (int i = 0; i < partite.Count; i++)
+			{	
+				somma += (partite[i].GolCasa + partite[i].GolOspite);
+
+				if (temp < (partite[i].GolCasa + partite[i].GolOspite))
+				{
+					temp = (partite[i].GolCasa + partite[i].GolOspite);
+					maggiore = partite[i];
+				}
+			}
+
+			int maxGol = score.Max();
+			int pos = score.IndexOf(maxGol);
+			migliore = squadre[pos];
+
+			MessageBox.Show($"La squadra che ha segnato il maggior numero di gol è: {migliore} {Environment.NewLine}Il numero di gol totale segnati nel campionato è: {somma} {Environment.NewLine}La partita in cui sono stati segnati più gol è:{Environment.NewLine}{maggiore.Stampa()}");
+		}
+
+		private void btnFiltra_Click(object sender, EventArgs e)
+		{
+			string dafiltrare = txtFiltra.Text;
+
+			lstPartite.Items.Clear();
+
+			if (string.IsNullOrEmpty(dafiltrare))
+			{
+				for (int i = 0; i < partite.Count; i++)
+				{
+					lstPartite.Items.Add(partite[i].Stampa());
+				}
+			}
+			else
+			{
+				for (int i = 0; i < partite.Count; i++)
+				{
+					if (partite[i].Casa == dafiltrare || partite[i].Ospite == dafiltrare)
+					{
+						lstPartite.Items.Add(partite[i].Stampa());
+					}
+				}
+			}
+		}
+	}
+}
