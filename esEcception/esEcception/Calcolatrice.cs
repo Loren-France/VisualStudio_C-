e778ecad43f002e2852b47e4d62aa7d1552c2f1c
Loren-France <14529846@itisrossi.vi.it>
2026-04-22 18:00:12 +0200

    Prova delle eccezioni

---
+++ esEcception/esEcception/Calcolatrice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace esEcception
+{
+	internal class Calcolatrice
+	{
+		private double a;
+		private double b;
+
+		public Calcolatrice(double a, double b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+
+		public double Dividi(int a, int b)
+		{
+			try
+			{
+				if (a < 0 || b < 0)
+				{
+					throw new ArgumentException();
+				}
+				return a / b;
+			}
+			catch (DivideByZeroException z)
+			{
+				Console.WriteLine(z.Message);
+			}
+			catch (ArgumentException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			catch
+			{
+				Console.WriteLine("Errore generico");
+			}
+			finally 
+			{
+				Console.WriteLine("Operazione terminata");
+			}
+			return 0;
+		}
+	}
+}
---
+++ esEcception/esEcception/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace esEcception
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Inserire il dividendo: ");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Inserire il divisore: ");
+			int num2 = int.Parse(Console.ReadLine());
+			Calcolatrice calcolatrice = new Calcolatrice(num1, num2);
+			double result = calcolatrice.Dividi(num1, num2);
+			Console.WriteLine($"Risultato della divisione: {result}");
+		}
+	}
+}
